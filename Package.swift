838151763a2b2ead0579712a0876bf44705548c8
Stefano Rizzetto <stefano@airbagstudio.it>
2025-04-04 16:08:06 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -3,11 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "CHDF5",
+    /*
     products: [
             .library(name: "libhdf5", targets: ["libhdf5"]),
         ],
         targets: [
             .systemLibrary(name: "libhdf5", path: "."),
         ]
-    //pkgConfig: "libhdf5"
+     */
+    pkgConfig: "libhdf5"
 )
