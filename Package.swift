c12ce55a15226c5caf1b4cf1d02b7527856a81a1
Stefano Rizzetto <stefano@airbagstudio.it>
2025-04-08 10:15:30 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -1,15 +1,31 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
     name: "CHDF5",
-    /*
+    
     products: [
-            .library(name: "libhdf5", targets: ["libhdf5"]),
-        ],
-        targets: [
-            .systemLibrary(name: "libhdf5", path: "."),
-        ]
-     */
-    pkgConfig: "libhdf5"
+        .library(name: "CHDF5", targets: ["CHDF5"]),
+    ],
+    targets: [
+        .target(name: "CHDF5",
+                path: ".",
+                publicHeadersPath: ".",
+                //publicHeadersPath: "/opt/homebrew/Cellar/hdf5/1.14.6/include",
+                cSettings: [
+                                .headerSearchPath("/opt/homebrew/Cellar/hdf5/1.14.6/include")
+                            ],
+                            linkerSettings: [
+                                .unsafeFlags(["-L/opt/homebrew/Cellar/hdf5/1.14.6/lib", "-lhdf5"])
+                            ]
+               )
+        /*
+        .systemLibrary(
+            name: "libhdf5",
+            path: "/opt/homebrew/Cellar/hdf5/1.14.6/lib",
+           
+            pkgConfig: "/opt/homebrew/Cellar/hdf5/1.14.6/lib/pkgconfig/hdf5.pc"
+        ),
+         */
+    ]
 )
